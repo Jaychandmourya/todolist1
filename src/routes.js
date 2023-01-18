dcb763fe4d6f6ffc5693e6c4e1cc677e8992efc0
Zignuts Technolab <ztlab116@Zignutss-Mac-mini.local>
2023-01-18 11:25:08 +0530

    todolist1.0

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import ToDoHome from "./components/ToDoHome.vue";
+// import AddToListVue from "./components/AddToList.vue";
+
+export default [
+  { path: "/", component: ToDoHome },
+  //   { path: "/AddTask", component: AddToListVue },
+];
